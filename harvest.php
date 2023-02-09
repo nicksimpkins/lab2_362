2959b3925bde495d4609e583bd6ce422b96e9129
Nick Simpkins <nsimpkins@hawk.iit.edu>
2023-02-09 13:32:16 -0600

    Added lab

---
+++ harvest.php
@@ -0,0 +1,11 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    function get_data() {
+        $email = $_REQUEST['email'];
+        $phonenum = $_REQUEST['phoneNum'];
+        $bday = $_REQUEST['bDay'];
+    }
+}
+
+?>
